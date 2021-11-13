dd1809f6ec8a5d9ee4a5eacb405497f2c39c9fbf
Kessler1459 <kessler1459@gmail.com>
2021-11-13 16:31:24 -0300

    models

--- api/djangoproy/settings.py
+++ api/djangoproy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'djangoproy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo',
+        'USER': 'dbuser',
+        'PASSWORD': 'pass',
+        'HOST': '192.168.0.4',
+        'PORT': '3306',
     }
 }
 
--- api/todo/models.py
+++ api/todo/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    owner = models.ForeignKey(User,on_delete=CASCADE)
+
+
+
+class Column(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    board = models.ForeignKey(Board, on_delete=CASCADE)
+
+
+class Note(models.Model):
+    class State(models.TextChoices):
+        TODO = 'FR', _('To do')
+        INPROGRESS = 'IP', _('In progress')
+        DONE = 'DN', _('Done')
+
+    state = models.CharField(
+        max_length=2,
+        choices=State.choices,
+        default=State.TODO,
+    )
+    name = models.CharField(max_length=70, blank=False)
+    description = models.CharField(max_length=300)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    board = models.ForeignKey(Board,on_delete=CASCADE)
+    note = models.ManyToManyField(Note)
